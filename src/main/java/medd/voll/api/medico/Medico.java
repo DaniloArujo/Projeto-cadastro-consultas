cfc8991a4210830b8a5a233a475c43900df34b3c
danilo <daniloaraujomota@gmail.com>
2025-07-22 11:33:40 -0300

    alter table medicos

--- src/main/java/medd/voll/api/medico/Medico.java
+++ src/main/java/medd/voll/api/medico/Medico.java
@@ -20,6 +20,7 @@ public class Medico {
     private Long id;
     private String nome;
     private String email;
+    private String telefone;
     private String crm;
 
     @Enumerated(EnumType.STRING)
@@ -29,9 +30,10 @@ public class Medico {
     private Endereco endereco;
 
     public Medico(DadosCadastroMedico dados) {
-        this.nome = dados.nome();;
+        this.nome = dados.nome();
         this.email = dados.email();
         this.crm = dados.crm();
+        this.telefone = dados.telefone();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
     }
